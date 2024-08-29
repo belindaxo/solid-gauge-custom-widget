96492ebcf64fdffbcbf8ff350854892f4c63d924
Belinda <bmdebruyn@gmail.com>
2024-08-29 12:48:10 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -6,23 +6,22 @@ Exporting(Highcharts);
 import SolidGauge from 'highcharts/modules/solid-gauge';
 SolidGauge(Highcharts);
 
-(function() {
-
-    const parseMetadata = metadata => {
-        const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata;
-        const dimensions = [];
-        for (const key in dimensionsMap) {
-            const dimension = dimensionsMap[key];
-            dimensions.push({ key, ...dimension });
-        }
-        const measures = [];
-        for (const key in measuresMap) {
-            const measure = measuresMap[key];
-            measures.push({ key, ...measure });
-        }
-        return { dimensions, measures, dimensionsMap, measuresMap };
+const parseMetadata = metadata => {
+    const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata;
+    const dimensions = [];
+    for (const key in dimensionsMap) {
+        const dimension = dimensionsMap[key];
+        dimensions.push({ key, ...dimension });
     }
+    const measures = [];
+    for (const key in measuresMap) {
+        const measure = measuresMap[key];
+        measures.push({ key, ...measure });
+    }
+    return { dimensions, measures, dimensionsMap, measuresMap };
+}
 
+(function() {
     class SolidGauge extends HTMLElement {
         constructor() {
             super();
