33d13cf6d9d9c9669348c1bb0165a81b4daf4b0f
Belinda <bmdebruyn@gmail.com>
2024-08-29 11:28:51 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,3 +5,141 @@ import Exporting from 'highcharts/modules/exporting';
 Exporting(Highcharts);
 import SolidGauge from 'highcharts/modules/solid-gauge';
 SolidGauge(Highcharts);
+
+(function() {
+
+    const parseMetadata = metadata => {
+        const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata;
+        const dimensions = [];
+        for (const key in dimensionsMap) {
+            const dimension = dimensionsMap[key];
+            dimensions.push({ key, ...dimension });
+        }
+        const measures = [];
+        for (const key in measuresMap) {
+            const measure = measuresMap[key];
+            measures.push({ key, ...measure });
+        }
+        return { dimensions, measures, dimensionsMap, measuresMap };
+    }
+
+    class SolidGauge extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+            this.shadowRoot.innerHTML = `
+                <div id="container" style="width:100%; height:100%;"></div>
+            `;
+        }
+
+        onCustomWidgetResize(width, height) {
+            this._renderChart();
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) {
+            this._renderChart();
+        }
+
+        onCustomWidgetDestroy() {
+            if (this._chart) {
+                this._chart.destroy();
+                this._chart = null;
+            }
+        }
+
+        _renderChart() {
+            const dataBinding = this.dataBinding;
+
+            if (!dataBinding || dataBinding.state !== 'success') {
+                return;
+            }
+
+            const { data, metadata } = dataBinding;
+            const { dimensions, measures } = parseMetadata(metadata);
+
+            const min = -1;
+            const max = 1;
+
+            const categoryData = [];
+
+            const series = measures.map(measure => {
+                return {
+                    id: measure.id,
+                    name: measure.label,
+                    data: [],
+                    key: measure.key,
+                    dataLabels: {
+                        format: 
+                        '<div style="text-align:center">' +
+                        '<span style="font-size:25px">{y}%</span><br/>'
+                    },
+                    type: 'solidgauge'
+                }
+            });
+
+            data.forEach(row => {
+                categoryData.push(dimensions.map(dimension => {
+                    return row[dimension.key].label;
+                }).join('/'));
+                series.forEach(series => {
+                    series.data.push(row[series.key].raw);
+                });
+            });
+
+            const chartOptions = {
+                chart: {
+                    type: 'solidgauge',
+                },
+                title: null,
+                pane: {
+                    center: ['50%', '85%'],
+                    size: '140%',
+                    startAngle: -90,
+                    endAngle: 90,
+                    background: {
+                        backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#fafafa',
+                        borderRadius: 5,
+                        innerRadius: '60%',
+                        outerRadius: '100%',
+                        shape: 'arc'
+                    }
+                },
+                tooltip: {
+                    enabled: false
+                },
+                yAxis: {
+                    min: min,
+                    max: max,
+                    stops: [
+                        [0.1, '#DF5353'], // red
+                        [0.5, '#DDDF0D'], // yellow
+                        [0.9, '#55BF3B'] // green
+                    ],
+                    lineWidth: 0,
+                    tickWidth: 0,
+                    minorTickInterval: null,
+                    tickAmount: 2,
+                    title: {
+                        y: -70
+                    },
+                    labels: {
+                        y: 16
+                    }
+                },
+                plotOptions: {
+                    solidgauge: {
+                        borderRadius: 3,
+                        dataLabels: {
+                            y: 5, 
+                            borderWidth: 0,
+                            useHTML: true
+                        }
+                    }
+                },
+                series
+            }
+            this._chart = Highcharts.chart(this.shadowRoot.getElementById('container'), chartOptions);
+        }
+    }
+    customElements.define('com-sap-sample-solidgauge', SolidGauge);
+})
